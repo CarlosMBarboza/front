7ef9cbd945bfa39d39290c874ccb5d527c7473f5
CarlosMBarboza <techosunion@hotmail.com>
2023-10-24 20:37:41 -0300

    Semana-3

--- Semana-2/clase-4.js
+++ Semana-2/clase-4.js
@@ -12,9 +12,9 @@ let titulos = document.querySelectorAll("article h2")
 // console.log(titulos);
 /* ---------------------------- Editado los ITEMS --------------------------- */
 // 👇acá podemos ver todas las propiedades CSS que podemos modificar con JS
-// console.log(menuItems);
-// console.log(menuItems[0]);
-// console.log(menuItems[0].style);
+console.log(menuItems);
+ console.log(menuItems[0]);
+console.log(menuItems[1].style);
 // agregamos manualmente nuevos estilos en el menú
 // menuItems.forEach(function (item) {
 //     item.style.textTransform = "uppercase"
@@ -33,25 +33,25 @@ menuItems.forEach(item => {
 /* ----------------------------- Editando clases ---------------------------- */
 
 // vamos probando uno a uno los métodos
-// console.log(sitio.classList);
+       // console.log(sitio.classList);
 
-// console.log(sitio.classList.contains("dark"));
-// console.log(sitio.classList);
+       // console.log(sitio.classList.contains("dark"));
+        //console.log(sitio.classList);
 
 
-// console.log(sitio.classList.add("dark"));
-// console.log(sitio.classList.contains("dark"));
+       // console.log(sitio.classList.add("dark"));
+        //console.log(sitio.classList.contains("dark"));
 
-// console.log(sitio.classList.remove("dark"));
-// console.log(sitio.classList.remove("remove"));
-// console.log(sitio.classList.contains("dark"));
-// console.log(sitio.classList);
+        //console.log(sitio.classList.remove("dark"));
+        //console.log(sitio.classList.remove("remove"));
+       // console.log(sitio.classList.contains("dark"));
+       // console.log(sitio.classList);
 
-// console.log(sitio.classList.toggle("dark"));
-// console.log(sitio.classList.toggle("dark"));
-// console.log(sitio.classList.toggle("dark"));
-// btnTema.textContent = "Cambiar a modo claro 🌞"
-// btnTema.innerText = "Cambiar a modo oscuro 🌜"
+        //console.log(sitio.classList.toggle("dark"));
+        //console.log(sitio.classList.toggle("dark"));
+        //console.log(sitio.classList.toggle("dark"));
+       // btnTema.textContent = "Cambiar a modo claro 🌞"
+        //btnTema.innerText = "Cambiar a modo oscuro 🌜"
 
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */
@@ -61,7 +61,24 @@ menuItems.forEach(item => {
 // 2- Debemos preguntarle al usuario mediante un confirm si desea usar el modo oscuro.
 // 3- Si el usuario confirma debemos aplicar la clase "dark" al "sitio", si cancela debe quedar en modo claro.
 // 4- A su vez,si el texto del boton debe decir "Cambiar a modo claro 🌞". De lo contrario, si está en modo claro debeb decir "Cambiar a modo oscuro 🌛"
+
+
+let modoOscuro = false;
+btnTema.addEventListener( 'click',( )=>{
+    modoOscuro = !modoOscuro;
+    elegirTema();
+} );
+
 function elegirTema() {
+    if (modoOscuro){
+        sitio.classList.add("dark");
+        modoOscuro=true;
+        btnTema.innerHTML="Cambiar a modo claro 🌞";
+        }else{
+            sitio.classList.remove("dark");
+            modoOscuro=false;
+            btnTema.innerHTML="Cambiar a modo oscuro 🌜";
+            };
 
 }
-elegirTema();
+
